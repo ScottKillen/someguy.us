3a0a2a1ca7567d8a82ac5c4e72f891daf9d79cb0
Scott Killen <scott@scottkillen.com>
2024-12-10 20:34:22 -0500

    Use tootpick for mastodon share function

--- content/_data/passthroughFiles.js
+++ content/_data/passthroughFiles.js
@@ -2,4 +2,5 @@ module.exports = {
   'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js':
     'js/bootstrap.bundle.min.js',
   'node_modules/jquery/dist/jquery.min.js': 'js/jquery.min.js',
+  'src/utils/tootpick.html': 'tootpick/index.html',
 };
